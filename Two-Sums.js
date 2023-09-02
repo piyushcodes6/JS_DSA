065a47a06e8db31c613d19986cec9f333d066c35
Piyush Goswami <piyushcodes6@Piyushs-MacBook-Pro.local>
2023-09-02 13:32:38 +0530

    Updated the Two sum to be more optimized

--- Two-Sums.js
+++ Two-Sums.js
@@ -5,16 +5,14 @@
 //Input :nums=[2,7,11,15], target =9
 //Output : [0,1](Because nums[0] + nums[1] ==9, so we return [0,1])
 
-//Brute Force Solution
+//Brute Force Solution to use all the possible combinations to get answers
 const twoSum=(nums,target)=>{
     let result=[];
     for (let i = 0; i < nums.length; i++) {
         for (let p = i+1; p < nums.length; p++) {
             console.log('i :'+i );
             console.log('p :'+p);
-            if(nums[i] + nums[p] ===target){
-                result.push(i,p)
-        }   
+            if(nums[i] + nums[p] ===target) result.push(i,p);
         }
     }
 
